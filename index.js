036e4f661408d92080facb24efba87081c1ef529
SHIVANGI  TIWARI <69473435+Shivangi708@users.noreply.github.com>
2021-10-06 18:02:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,135 +1,160 @@
-var character = document.querySelector(".character");
-var map = document.querySelector(".map");
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');
+const clearBtn = document.querySelector('.clear-tasks');
+// const remove = document.querySelectorAll('a');
 
-//start in the middle of the map
-var x = 90;
-var y = 34;
-var held_directions = []; //State of which arrow keys we are holding down
-var speed = 1; //How fast the character moves in pixels per frame
+loadAllEventListener();
 
-const placeCharacter = () => {
-   
-   var pixelSize = parseInt(
-      getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
-   );
-   
-   const held_direction = held_directions[0];
-   if (held_direction) {
-      if (held_direction === directions.right) {x += speed;}
-      if (held_direction === directions.left) {x -= speed;}
-      if (held_direction === directions.down) {y += speed;}
-      if (held_direction === directions.up) {y -= speed;}
-      character.setAttribute("facing", held_direction);
-   }
-   character.setAttribute("walking", held_direction ? "true" : "false");
-   
-   //Limits (gives the illusion of walls)
-   var leftLimit = -8;
-   var rightLimit = (16 * 11)+8;
-   var topLimit = -8 + 32;
-   var bottomLimit = (16 * 7);
-   if (x < leftLimit) { x = leftLimit; }
-   if (x > rightLimit) { x = rightLimit; }
-   if (y < topLimit) { y = topLimit; }
-   if (y > bottomLimit) { y = bottomLimit; }
-   
-   
-   var camera_left = pixelSize * 66;
-   var camera_top = pixelSize * 42;
-   
-   map.style.transform = `translate3d( ${-x*pixelSize+camera_left}px, ${-y*pixelSize+camera_top}px, 0 )`;
-   character.style.transform = `translate3d( ${x*pixelSize}px, ${y*pixelSize}px, 0 )`;  
+function loadAllEventListener(){
+    //DOM load event
+    document.addEventListener('DOMContentLoaded', getTasks);
+    //add task
+    form.addEventListener('submit', addTask);
+    //remove task
+    taskList.addEventListener('click',removeTask);
+    //clear all task
+    clearBtn.addEventListener('click', clearTasks);
+    //filter event
+    filter.addEventListener('keyup', filterTask);
 }
 
+function getTasks(){
+    let tasks;
 
-//Set up the game loop
-const step = () => {
-   placeCharacter();
-   window.requestAnimationFrame(() => {
-      step();
-   })
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task){
+        const li = document.createElement('li');
+
+    li.className = 'collection-item';
+
+    li.appendChild(document.createTextNode(task));
+
+    const link = document.createElement('a');
+
+    link.className = 'delete-item secondary-content';
+
+    link.id = 'delete-item';
+
+    link.innerHTML = "<i class='fa fa-remove'></i>";
+
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+    })    
 }
-step(); //kick off the first step!
 
+function addTask(e){
 
+    if(taskInput.value === '')
+    {
+        alert('Add a Task');
+    }
 
-/* Direction key state */
-const directions = {
-   up: "up",
-   down: "down",
-   left: "left",
-   right: "right",
+    const li = document.createElement('li');
+
+    li.className = 'collection-item';
+
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    const link = document.createElement('a');
+
+    link.className = 'delete-item secondary-content';
+
+    link.id = 'delete-item';
+
+    link.innerHTML = "<i class='fa fa-remove'></i>";
+
+    li.appendChild(link);
+
+    taskList.appendChild(li);
+
+    storeTaskInLocalStorage(taskInput.value);
+
+    taskInput.value = '';
+
+    e.preventDefault();
 }
-const keys = {
-   38: directions.up,
-   37: directions.left,
-   39: directions.right,
-   40: directions.down,
+
+function storeTaskInLocalStorage(task){
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
-document.addEventListener("keydown", (e) => {
-   var dir = keys[e.which];
-   if (dir && held_directions.indexOf(dir) === -1) {
-      held_directions.unshift(dir)
-   }
-})
 
-document.addEventListener("keyup", (e) => {
-   var dir = keys[e.which];
-   var index = held_directions.indexOf(dir);
-   if (index > -1) {
-      held_directions.splice(index, 1)
-   }
-});
+function clearTasks(e){
 
+    // taskList.innerHTML = '';  method 1 (average)
 
+    // taskList.remove();   method 2 (worst)
 
-/* BONUS! Dpad functionality for mouse and touch */
-var isPressed = false;
-const removePressedAll = () => {
-   document.querySelectorAll(".dpad-button").forEach(d => {
-      d.classList.remove("pressed")
-   })
+    // window.location.reload();
+
+    while(taskList.firstChild){        //method 3 (best**)
+        taskList.removeChild(taskList.firstChild);  
+    }   
+
+    localStorage.clear();
+
+    e.preventDefault();
 }
-document.body.addEventListener("mousedown", () => {
-   console.log('mouse is down')
-   isPressed = true;
-})
-document.body.addEventListener("mouseup", () => {
-   console.log('mouse is up')
-   isPressed = false;
-   held_directions = [];
-   removePressedAll();
-})
-const handleDpadPress = (direction, click) => {   
-   if (click) {
-      isPressed = true;
-   }
-   held_directions = (isPressed) ? [direction] : []
-   
-   if (isPressed) {
-      removePressedAll();
-      document.querySelector(".dpad-"+direction).classList.add("pressed");
-   }
+
+function removeTask(e){
+
+    if(e.target.parentElement.classList.contains('delete-item'))
+    {
+        e.target.parentElement.parentElement.remove();
+    }
+
+    remaoveTaskFromLocalStorage(e.target.parentElement.parentElement);
+
+    e.preventDefault();
 }
-//Bind a ton of events for the dpad
-document.querySelector(".dpad-left").addEventListener("touchstart", (e) => handleDpadPress(directions.left, true));
-document.querySelector(".dpad-up").addEventListener("touchstart", (e) => handleDpadPress(directions.up, true));
-document.querySelector(".dpad-right").addEventListener("touchstart", (e) => handleDpadPress(directions.right, true));
-document.querySelector(".dpad-down").addEventListener("touchstart", (e) => handleDpadPress(directions.down, true));
 
-document.querySelector(".dpad-left").addEventListener("mousedown", (e) => handleDpadPress(directions.left, true));
-document.querySelector(".dpad-up").addEventListener("mousedown", (e) => handleDpadPress(directions.up, true));
-document.querySelector(".dpad-right").addEventListener("mousedown", (e) => handleDpadPress(directions.right, true));
-document.querySelector(".dpad-down").addEventListener("mousedown", (e) => handleDpadPress(directions.down, true));
+function remaoveTaskFromLocalStorage(taskItem){
+    let tasks;
 
-document.querySelector(".dpad-left").addEventListener("mouseover", (e) => handleDpadPress(directions.left));
-document.querySelector(".dpad-up").addEventListener("mouseover", (e) => handleDpadPress(directions.up));
-document.querySelector(".dpad-right").addEventListener("mouseover", (e) => handleDpadPress(directions.right));
-document.querySelector(".dpad-down").addEventListener("mouseover", (e) => handleDpadPress(directions.down));
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
 
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index,1);
+        }
+    });
 
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
+function filterTask(e){
 
+    const text = e.target.value.toLowerCase();
 
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    })   
 
-
+    e.preventDefault();
+}
